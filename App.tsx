a361fb9989e7aed239c96c07313dfc2b10e01584
Jott4 <jvgcunha2002@gmail.com>
2022-04-30 16:57:17 -0300

    removed firebase config

--- App.tsx
+++ App.tsx
@@ -9,12 +9,12 @@ import DrawerNavigation from "./src/navigation/DrawerNavigation";
 import InititalNavigation from "./src/navigation/InititalNavigation";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXbeyUqGOfSifIg7s57F6bhaB8iyepAWE",
-  authDomain: "mind-booster-b51c1.firebaseapp.com",
-  projectId: "mind-booster-b51c1",
-  storageBucket: "mind-booster-b51c1.appspot.com",
-  messagingSenderId: "608274014813",
-  appId: "1:608274014813:web:e3df6531985da15d0de3b4",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 initializeApp(firebaseConfig);
---
+++ readme.md
@@ -0,0 +1,5 @@
+para rodar o projeto
+
+- adicione firebaseConfig
+- yarn
+- yarn start
